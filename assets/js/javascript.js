b49a946a5659978945a4a4456f5d4c07d29b2b1d
juicyjake12 <108505695+juicyjake12@users.noreply.github.com>
2022-08-26 00:51:47 -0400

    added working api and text to page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,28 @@
-https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+var weather = {
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=df430eaf4efe581c905ef93ff158bf76")
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { temp, humidity } = data.main;
+        const { name } = data;
+         const { speed } = data.wind;
+       const { icon, description } = data.weather[0];
+        document.querySelector("#description").innerText = description;
+        document.querySelector("#temp").innerText = temp + "°f";
+        document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector("#wind").innerText = "wind speed: " + speed + "mph";
+        document.querySelector("#city").innerText = "weather in " + name;
+        document.querySelector("#humidity").innerText = "humidity level:" + humidity + "%";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".searchBar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
